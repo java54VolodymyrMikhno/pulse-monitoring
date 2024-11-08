7fe23df8e2461dc197a26429de33bff160db6d68
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-11-08 02:47:50 +0200

    added new lambda function range-provider

--- pulse-values-analyzer/src/main/java/telran/pulse/monitoring/App.java
+++ pulse-values-analyzer/src/main/java/telran/pulse/monitoring/App.java
@@ -1,7 +1,14 @@
 package telran.pulse.monitoring;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.*;
 import java.util.logging.*;
+
+import org.json.JSONObject;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
@@ -12,13 +19,16 @@ import software.amazon.awssdk.services.dynamodb.model.PutItemRequest.Builder;
 import static telran.pulse.monitoring.Constants.*;
 
 public class App {
+	record Range(int min, int max) {
+	}
 
 	static DynamoDbClient client = DynamoDbClient.builder().build();
 	static Builder request;
 	static Logger logger = Logger.getLogger("pulse-value-analyzer");
+	static String rangeProviderUrl = System.getenv("RANGE_PROVIDER_URL");
 	static {
 		loggerSetUp();
-		
+
 	}
 
 	public void handleRequest(DynamodbEvent event, Context context) {
@@ -47,7 +57,7 @@ public class App {
 
 	private static Level getLoggerLevel() {
 		String levelStr = System.getenv()
-		.getOrDefault(LOGGER_LEVEL_ENV_VARIABLE, DEFAULT_LOGGER_LEVEL);
+				.getOrDefault(LOGGER_LEVEL_ENV_VARIABLE, DEFAULT_LOGGER_LEVEL);
 		Level res = null;
 		try {
 			res = Level.parse(levelStr);
@@ -59,10 +69,31 @@ public class App {
 
 	private void processPulseValue(Map<String, AttributeValue> map) {
 		int value = Integer.parseInt(map.get(VALUE_ATTRIBUTE).getN());
+		int patientId = Integer.parseInt(map.get(PATIENT_ID_ATTRIBUTE).getN());
+
 		logger.finer(getLogMessage(map));
-		if (value > MAX_THRESHOLD_PULSE_VALUE || value < MIN_THRESHOLD_PULSE_VALUE) {
+		Range range = getRange(patientId);
+		if (range != null && value > range.max || value < range.min) {
 			processAbnormalPulseValue(map);
 		}
+	}
+
+	private Range getRange(int patientId) {
+		HttpClient client = HttpClient.newHttpClient();
+		String url = String.format("%s?patientId=%d", rangeProviderUrl, patientId);
+		HttpRequest request = HttpRequest.newBuilder(URI.create(url)).build();
+		try {
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			if (response.statusCode() == 200) {
+				JSONObject json = new JSONObject(response.body());
+				return new Range(json.getInt("min"), json.getInt("max"));
+			} else {
+				throw new RuntimeException("Failed to fetch range: " + response.body());
+			}
+		} catch (IOException | InterruptedException e) {
+			throw new RuntimeException("Error fetching range", e);
+		}
+
 	}
 
 	private void processAbnormalPulseValue(Map<String, AttributeValue> map) {
